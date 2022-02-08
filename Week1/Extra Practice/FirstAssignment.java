e8cb121ea2346e75bdc2151b920c9196177871d5
PHONG  CHAU <main@PHONGs-MacBook-Air.local>
2022-02-07 22:34:17 -0800

    FirstAssigment-Added 'my'

--- Week1/Extra Practice/FirstAssignment.java
+++ Week1/Extra Practice/FirstAssignment.java
@@ -2,7 +2,7 @@ public class FirstAssignment {
     public static void main(String[] args) {
         System.out.println("My name is Coding Dojo");
         System.out.println("I am 100 years old");
-        System.out.println("hometown is Burbank, CA");
+        System.out.println("My hometown is Burbank, CA");
 
     }
 }
