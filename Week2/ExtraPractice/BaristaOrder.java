342af05ea75ed0cdc0d7efd5aab33f2d626e2933
PHONG  CHAU <main@PHONGs-MacBook-Air.local>
2022-02-16 08:36:38 -0800

    add Brista assignment

--- Week2/ExtraPractice/BaristaOrder.java
+++ Week2/ExtraPractice/BaristaOrder.java
@@ -4,6 +4,7 @@ public class BaristaOrder {
     private String name;
     private boolean ready;
     private ArrayList<BaristaItem> items;
+    private double total;
 
     public BaristaOrder() {
         this.ready = false;
@@ -18,4 +19,35 @@ public class BaristaOrder {
         this.items = new ArrayList<BaristaItem>();
 
     }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+
+    }
+
+    public void addItem(BaristaItem parm) {
+        this.items.add(parm);
+    }
+
+    public void displayOrder() {
+    }
+
+    public void getOrderTotal() {
+        System.out.printf("Customer Name: %s \n", this.name);
+        for (BaristaItem i : this.items) {
+            this.total += i.getPrice();
+            System.out.printf("%s: $%.2f %n", i.getName(), i.getPrice());
+        }
+        System.out.printf("Order total is: $ %.2f %n", this.total);
+    }
+
+    public String getStatusMessage() {
+        System.out.printf("%s, ", this.name);
+        if (this.ready == true) {
+            return "your order is ready :)";
+        } else {
+            return "thank you for waiting. Your order will be ready soon :(.";
+        }
+    }
+
 }
--- Week2/ExtraPractice/BaristaTest.java
+++ Week2/ExtraPractice/BaristaTest.java
@@ -1,5 +1,44 @@
 public class BaristaTest {
     public static void main(String[] args) {
+        BaristaItem item1 = new BaristaItem("Coffee", 3);
+        BaristaItem item2 = new BaristaItem("Milk", 2.75);
+        BaristaItem item3 = new BaristaItem("Cappicino", 4.5);
+        BaristaItem item4 = new BaristaItem("Latte", 5);
+        BaristaItem item5 = new BaristaItem("OJ", 2);
+
+        BaristaOrder order1 = new BaristaOrder("Brad");
+        BaristaOrder order2 = new BaristaOrder("George");
+        BaristaOrder order3 = new BaristaOrder("Don");
+        BaristaOrder order4 = new BaristaOrder("Matt");
+        BaristaOrder order5 = new BaristaOrder("");
+
+        order1.addItem(item2);
+        order1.addItem(item1);
+        order1.addItem(item3);
+        order1.displayOrder();
+        order1.setReady(true);
+        order1.getOrderTotal();
+        System.out.println(order1.getStatusMessage());// ready
+
+        order3.addItem(item5);
+        order3.addItem(item4);
+        order3.addItem(item1);
+        order3.displayOrder();
+        order3.setReady(true);
+        order3.getOrderTotal();
+
+        System.out.println(order3.getStatusMessage());// ready
+
+        order2.addItem(item2);
+        order2.addItem(item3);
+        order2.addItem(item2);
+        order2.displayOrder();
+        order2.setReady(false);
+        order2.getOrderTotal();
+
+        System.out.println(order2.getStatusMessage());// ready
+
+        System.out.println("----------------");
 
     }
 
